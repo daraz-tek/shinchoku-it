ed6a46da5ccc1b5a6c8007c4073bfd44a2f71595
sakaguchi <takashi.sakaguchi@ummm.info>
2016-06-14 19:41:00 +0900

    added association Team.timelines

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
   has_many :users, inverse_of: :team, dependent: :destroy
+  has_many :timelines, -> { published.order(id: :desc) }, through: :users, source: :reports
   validates :uid, presence: true, uniqueness: true
 end
