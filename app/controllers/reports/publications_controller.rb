5f8f58974ccb78463c3857860fdb67642e42de3c
sakaguchi <takashi.sakaguchi@ummm.info>
2016-06-14 20:49:04 +0900

    fix typo and i18n

--- app/controllers/reports/publications_controller.rb
+++ app/controllers/reports/publications_controller.rb
@@ -1,7 +1,7 @@
 class Reports::PublicationsController < ApplicationController
   login_required
 
-  # GET /reports/drafts
+  # GET /reports/publications
   def index
     @reports = current_user.reports.published
     render 'reports/index'
